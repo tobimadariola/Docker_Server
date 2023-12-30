8e1adffe1f664dd19d360806326d11c84ecea1d0
haywise <152945861+haywise@users.noreply.github.com>
2023-12-29 21:01:59 -0600

    Update main.tf

--- remote-state/main.tf
+++ remote-state/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "Abdulhakeem-docker-tfstate"
+  bucket = "atrihomes-docker-tfstate"
      
   lifecycle {
     prevent_destroy = true
