862b50555f9137da19c1f4263417e20f00e56fd3
haywise <ajaoakeem200000@gmail.com>
2023-12-29 20:53:46 -0600

    change to my profile

--- remote-state/main.tf
+++ remote-state/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   region = "us-east-1"
-  profile= "yusuf"
+  profile= "Abdulhakeem"
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "yusuf-docker-tfstate"
+  bucket = "Abdulhakeem-docker-tfstate"
      
   lifecycle {
     prevent_destroy = true
